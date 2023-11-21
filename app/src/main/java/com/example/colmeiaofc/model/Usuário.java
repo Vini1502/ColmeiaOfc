89960a814d2cb47f54a56e06a247e77b00a77d9a
Vini1502 <v174711@dac.unicamp.br>
2023-11-21 14:09:03 -0300

    Adição da class usuário

---
+++ app/src/main/java/com/example/colmeiaofc/model/Usuário.java
@@ -0,0 +1,33 @@
+package com.example.colmeiaofc.model;
+
+public class Usuário {
+
+    private String nome,email,senha;
+
+    public Usuário() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
