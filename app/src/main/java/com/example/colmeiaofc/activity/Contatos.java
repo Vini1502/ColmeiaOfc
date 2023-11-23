50a2ec2062f7452e9ef8466528ec89cd0791086b
Vini1502 <v174711@dac.unicamp.br>
2023-11-22 22:14:59 -0300

    Adição de links

--- app/src/main/java/com/example/colmeiaofc/activity/Contatos.java
+++ app/src/main/java/com/example/colmeiaofc/activity/Contatos.java
@@ -13,11 +13,23 @@ import com.example.colmeiaofc.R;
 public class Contatos extends AppCompatActivity {
 
     private TextView txtInstagramLink;
+    private TextView mapsLink;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contatos);
         txtInstagramLink = findViewById(R.id.textInsta);
+        mapsLink = findViewById(R.id.textMaps);
+
+        mapsLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            String mapsUrl = "https://maps.app.goo.gl/9PkimyFmk71aN7LbA";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(mapsUrl));
+            startActivity(intent);
+            }
+        });
 
         // Adiciona um ouvinte de clique ao texto do Instagram
         txtInstagramLink.setOnClickListener(new View.OnClickListener() {
